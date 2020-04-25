239c68b628fe9d9f7b124eeff063db153a154290
Philip Choi <miphc42@users.noreply.github.com>
2020-04-25 19:44:59 -0400

    https://github.com/SaurontheMighty/devSocial

--- lib/paths/login.dart
+++ lib/paths/login.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-
 class Login extends StatefulWidget {
   @override
   _LoginState createState() => _LoginState();
@@ -8,17 +7,52 @@ class Login extends StatefulWidget {
 class _LoginState extends State<Login> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text(
-          'devSocial',
-          style: TextStyle(
-            fontWeight: FontWeight.bold
-          ),
-          ),
-        backgroundColor: Colors.redAccent,
-        automaticallyImplyLeading: false,
+    return  Scaffold(
+      body: SafeArea(
+        child: ListView(
+          padding: EdgeInsets.symmetric(horizontal: 18),
+          children: <Widget>[
+            Column(
+              children: <Widget>[
+                SizedBox(
+              height: 80,
+            ),
+            Image.asset('insert good logo'),
+            SizedBox(
+              height: 40,
+            ),
+            Text('Login',
+            style: TextStyle(fontSize: 40, color: Colors.red),
+            )
+              ],
+            ),
+            SizedBox(height: 70), 
+            TextField(
+              decoration: InputDecoration(
+                labelText: 'Username',
+                labelStyle: TextStyle(fontSize: 20),
+              )
+            ),
+             SizedBox(height: 25), 
+            TextField(
+              obscureText: true,
+              decoration: InputDecoration(
+                labelText: 'Password',
+                labelStyle: TextStyle(fontSize: 20),
+              )
+            ),
+            SizedBox(height: 30),
+            FlatButton(onPressed: (){ 
+              Navigator.pushNamed(context, '/home');}
+            , child: 
+            Text(
+              "Login"
+            ),
+            color: Colors.blue,)
+          ],
+        ),
       ),
+      
     );
   }
 }
