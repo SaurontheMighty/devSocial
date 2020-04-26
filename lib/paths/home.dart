7c3f6d33946bd317d9545257b8573d0cbd697234
Ashish <yesashishs@gmail.com>
2020-04-25 20:01:54 -0400

    Revert "Add TabBar"
    This reverts commit 1ee6bf49f96fd1129021e33c275e573d646d2854.

--- lib/paths/home.dart
+++ lib/paths/home.dart
@@ -8,58 +8,16 @@ class Home extends StatefulWidget {
 class _HomeState extends State<Home> {
   @override
   Widget build(BuildContext context) {
-    return new MaterialApp(
-      color: Colors.yellow,
-      home: DefaultTabController(
-        length: 4,
-        child: new Scaffold(
-          appBar: AppBar(
-            title: Text(
-              'devSocial',
-              style: TextStyle(
-                fontWeight: FontWeight.bold
-              ),
-            ),
-            backgroundColor: Colors.redAccent,
-            automaticallyImplyLeading: false,
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(
+          'devSocial',
+          style: TextStyle(
+            fontWeight: FontWeight.bold
           ),
-          body: TabBarView(
-            children: [
-              new Container(
-                color: Colors.redAccent,
-              ),
-              new Container(
-                color: Colors.orange,
-              ),
-              new Container(
-                color: Colors.lightGreen,
-              ),
-              new Container(
-                color: Colors.red,
-              ),
-            ],
           ),
-          bottomNavigationBar: new TabBar(
-          tabs: [
-            Tab(
-              icon: new Icon(Icons.home),
-            ),
-            Tab(
-              icon: new Icon(Icons.group),
-            ),
-            Tab(
-              icon: new Icon(Icons.chat_bubble),
-            ),
-            Tab(icon: new Icon(Icons.settings),)
-          ],
-          labelColor: Colors.redAccent,
-          unselectedLabelColor: Colors.black,
-          indicatorSize: TabBarIndicatorSize.label,
-          indicatorPadding: EdgeInsets.all(5.0),
-          indicatorColor: Colors.redAccent,
-          ),
-          backgroundColor: Colors.white,
-        ), 
+        backgroundColor: Colors.redAccent,
+        automaticallyImplyLeading: false,
       ),
     );
   }
